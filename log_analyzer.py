1add81edc8ad507f57cc9cb3f4a1776c28f0f74c
ladarius-dev <ladarius.cou@gmail.com>
2026-09-17 14:29:45 -0500

    Add failed login reporting

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,9 +2,11 @@ with open("sample.log", "r") as file:
     logs = file.readlines()
 
 failed_attempts = {}
+total_failed_attempts = 0
 
 for log in logs:
     if "FAILED" in log:
+        total_failed_attempts += 1
         parts = log.split()
         ip_address = parts[5].replace("ip=", "")
 
@@ -13,8 +15,12 @@ for log in logs:
         else:
             failed_attempts[ip_address] = 1
 
-print(failed_attempts)
+
+print("===== SECURITY LOG REPORT =====")
+print(f"Total Failed Login Attempts: {total_failed_attempts}")
 
 for ip_address, attempts in failed_attempts.items():
     if attempts >= 3:
         print(f"Suspicious IP: {ip_address} - {attempts} failed attempts")
+    else:
+        print(f"IP: {ip_address} - {attempts} failed attempt")
